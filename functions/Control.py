3737ef8c5d3bb5b132ea054f9b122741db2e608a
Evandro <evandroalves123321@gmail.com>
2026-09-04 17:19:30 -0300

    Update Control.py

--- functions/Control.py
+++ functions/Control.py
@@ -5,5 +5,17 @@ def end():
     print(exit())
     sys.exit(0)
 
+def proceed():
+    while True:
+        res = input("   Deseja fazer outra operação? (s/n) ")
+        if res in ("s", "sim", "yes", "ys", "y"):
+            break
+        elif res in ("n", "nao", "não", "no", "exit", "sair", "q"):
+            return end()
+        else:
+            print("   Informe uma opção válida!")
+            continue
+
 def start():
     print(menu())
+    
