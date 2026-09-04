2c2251fb1e3bf9119b30a02e62b785a276cdd254
Evandro <evandroalves123321@gmail.com>
2026-09-04 17:12:44 -0300

    Update Utils.py

---
+++ utils/Utils.py
@@ -0,0 +1,17 @@
+class Strings():
+    _Menu = """
+                ╔════════════════════════════════════════╗
+                ║                                        ║
+                ║       \033[34mB a s e 6 4   x   J s o n  \033[0m      ║
+                ║                                        ║
+                ╠════════════════════════════════════════╣
+                ║                                        ║
+                ║ • Coloque o arquivo na pasta inputs.   ║
+                ║                                        ║
+                ║                                        ║
+                ║ • Para sair digite (sair), (exit) ou   ║
+                ║  (q) quando quiser                     ║
+                ║                                        ║
+                ╚════════════════════════════════════════╝"""
+    
+    
