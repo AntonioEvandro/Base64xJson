1f1e640dce2e9dae5de40b6417b462d30a8f4cd4
Evandro <evandroalves123321@gmail.com>
2026-09-06 22:04:13 -0300

    Update Converter.py

--- functions/Converter.py
+++ functions/Converter.py
@@ -1,9 +1,27 @@
-import base64, json
-from utils.Utils import readInput, writeOutput, sucess
+from utils.Utils import readInput, b64ForJson, jsonForB64, jsonLoads, writeOutput, jsonDump, sucess
 
 def convert(file:str, format:int):
     """
         Lê o arquivo e exporta para o formato desejado ('json' ou 'base64').
     """
 
-    
+    data = readInput(file)
+
+    if format == -64:
+        try:
+            jsonData = b64ForJson(data)
+
+            jsonDump(file+".json",jsonData)
+            print(sucess())
+        except Exception as e:
+            print(f"[ERRO] Falha ao converter para JSON: {e}")
+
+    elif format == 64:
+        try:
+            jsonData = jsonLoads(data)
+            jsonStr = jsonForB64(jsonData)
+
+            writeOutput(file+".data", jsonStr)
+            print(sucess())
+        except Exception as e:
+            print(f"[ERRO] Falha ao converter para Base64: {e}")
